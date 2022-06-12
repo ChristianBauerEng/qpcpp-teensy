74a538f484a9306639c6f66d05f78842846400e6
Christian Bauer <christian.a.bauer@t-online.de>
2022-06-12 13:54:41 +0200

    More debug output for test runner

--- test/test_custom_runner.py
+++ test/test_custom_runner.py
@@ -1,10 +1,16 @@
+from cmath import e
+from logging import captureWarnings
+from os import popen
 from platformio.public import TestRunnerBase
 import click
 import string
-from subprocess import Popen, PIPE, CREATE_NEW_CONSOLE
+from subprocess import Popen, run, PIPE, CREATE_NEW_CONSOLE
 import socket
 import threading
 from time import sleep
+import sys
+import os
+import glob
 
 class CustomTestRunner(TestRunnerBase):
 
@@ -20,58 +26,100 @@ class CustomTestRunner(TestRunnerBase):
         return super().stage_uploading()
 
 
-    def stage_testing(self):
+    def start_qutest(self):
+        # Build list of test-scripts to execute for qutest.
+        test_dir = self.project_config.get("platformio", "test_dir")
+        test_name = self.test_suite.test_name
 
-        # Run qutest and connect to the qspy process on the default port.
-        click.secho("Running qutest...")
+        # find python files starting with "test_" in the directory of the current test.
+        files = glob.glob(os.path.join(test_dir, test_name, "test_*.py"))
+        click.secho("Found the following test files: {}".format(files), fg='green')
+        fileListString = ' '.join(files)
+
+        # Find test scripts, run qutest and connect to the qspy process on the default port.  
+        thisPath = os.path.dirname(os.path.realpath(__file__))
+        qutest_path = os.path.join(thisPath, 'qutest.py') 
+     
+        click.secho("Running qutest: {}".format(qutest_path))
         # qutest will write its output to PIPE, that we can read periodically.
         # However, since the reading is blocking, we will delegate that to another thread, from where the output
         # will be processed.
-        self.qutest_process = Popen('qutest', creationflags=CREATE_NEW_CONSOLE, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        self.qutest_process = Popen(['python', '-u', qutest_path, fileListString], stdout=PIPE, stderr=PIPE, creationflags=CREATE_NEW_CONSOLE, shell=True, universal_newlines=True)
 
-        # Start polling
-        self.qutest_polling_thread = threading.Thread(group=None, name="qutestPolling", target=qutest_polling_loop, args=(self,), daemon=True, universal_newlines=True)
+        # # Start polling
+        self.qutest_polling_thread = threading.Thread(group=None, name="qutestPolling", target=self.qutest_polling_loop)
+        self.qutest_polling_thread.start()
 
-        # wait for polling thread to end, which happens when qutest has stopped and all outputs have been processed.
-        self.qutest_polling_thread.join()
+        # # wait for polling thread to end, which happens when qutest has stopped and all outputs have been processed.
+        #self.qutest_polling_thread.join()
+
+        #click.echo("Polling-thread rejoined. Ending unit tests.")
 
         # TODO: Kill qspy-process and close console
 
+        #self.test_suite.on_finish()
+
+
+    def stage_testing(self):
+        # Pausing for a couple of seconds to let Teensy re-establish Serial connection.
+        # sleep(10)
+        self.isTesting = False
+        
         return super().stage_testing()
 
 
-        # Run this function in a dedicated thread to continously poll output from the qutest-process
-        # Each received line should correspond to a test result.
+    # Run this function in a dedicated thread to continously poll output from the qutest-process
+    # Each received line should correspond to a test result.
     def qutest_polling_loop(self):
-
+        click.secho("Starting polling of qutest...", fg='green')
         # Poll qutest output while the process is still alive.
         while self.qutest_process.poll() is None:
+            click.echo("Polling qutest...")
+            
             # Read all lines in STDOUT
-            outputs = self.qutest_process.stdout.readlines()
+            self.qutest_process.stdout.flush()
+            #self.qutest_process.stderr.flush()            
+            outputs = self.qutest_process.stdout.read()
+            #errors = self.qutest_process.stderr.read()
+
+            click.echo(outputs)
+            #click.secho(errors, fg='red')
 
             # Analyse each line individually.
-            for line in outputs:
-                click.echo(line)
-                # TODO: parse lines and add test-cases to the test-suite.
+            # for line in outputs:
+            #     click.echo(line)
+            #     # TODO: parse lines and add test-cases to the test-suite.
 
             # Wait for new data
-            sleep(0.5)
+            sleep(0.05)
 
+        out, err = self.qutest_process.communicate()
+        click.echo("qutest seems to have stopped. No longer polling")
+        click.echo(out)
+        click.secho(err, fg="red")
+        self.test_suite.on_finish()
         return
 
     # Processed data received from MCU via debug port.
     # Forwards the received data to the qspy-process.
     def on_testing_data_output(self, data):
-        click.echo(data)
-        self.qspy_socket.send(data)
-        #return super().on_testing_line_output(line)
+        click.secho("D: {}".format(data), fg='yellow')
+        self.qspy_socket.sendall(data)
+
+        if not self.isTesting:
+            self.start_qutest()
+            self.isTesting = True
+        return super().on_testing_data_output(data)
 
     # Called when receiving a complete line from the MCU.
     # Since qspy-produces binary data we can ignore this.
     def on_testing_line_output(self, line):
+        click.secho("L: {}".format(line), fg='yellow')
+        self.qspy_socket.send(bytes(line, "utf-8"))
         return
     
     def teardown(self):
-        self.qspy_socket.close()
-        self.qspy_process.terminate()
+        click.echo("tearing down tests...")
+        # self.qspy_socket.close()
+        # self.qspy_process.terminate()
         return super().teardown()
